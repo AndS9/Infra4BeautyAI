2680abfc6115b1d0ab9887bc09f8a53f34f3485f
Andrii Shukalo <shukaload@gmail.com>
2026-08-02 22:34:09 +0300

    fix start.sh

--- frontend_service/start.sh
+++ frontend_service/start.sh
@@ -4,6 +4,7 @@ set -e
 FRONT_DIR="/home/developer/frontend_service/beauty-ai-platform"
 
 if [ ! -d "$FRONT_DIR/.git" ]; then
+    mkdir -p /home/developer/frontend_service
     cd /home/developer/frontend_service
     git clone -b frontend https://github.com/KLUZOO/beauty-ai-platform
 else
