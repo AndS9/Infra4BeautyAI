42f076d6b2b8df6a7efde78b3e1c73d7941abe05
Andrii Shukalo <shukaload@gmail.com>
2026-08-13 23:18:02 +0300

    Fix get_keys script

--- scripts/get_server_secrets.py
+++ scripts/get_server_secrets.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 from azure.identity import DefaultAzureCredential
@@ -5,14 +6,27 @@ from azure.keyvault.secrets import SecretClient
 
 VAULT_URL = "https://keyvaultbeautycert.vault.azure.net/"
 
+def format_secret(content: str, header: str, footer: str) -> str:
+    body = content[len(header):-len(footer)]
+    body = body.replace(" ", "\n")
+    return header + body + footer
+
 if __name__ == "__main__":
     credential = DefaultAzureCredential()
     client = SecretClient(vault_url=VAULT_URL, credential=credential)
 
+    cert = client.get_secret("server-crt").value
+    key = client.get_secret("server-key").value
+    cert = format_secret(cert, "-----BEGIN CERTIFICATE-----", 
+                             "-----END CERTIFICATE-----")
+    
+    key = format_secret(key, "-----BEGIN PRIVATE KEY-----",
+                             "-----END PRIVATE KEY-----")
+    
     #Write secrets to the specified file
     with open("/etc/nginx/ssl/server.crt", "w") as f:
-        secret = client.get_secret("server-crt")
-        f.write(secret.value)
+        f.write(cert)
     with open("/etc/nginx/ssl/server.key", "w") as f:
-        secret = client.get_secret("server-key")
-        f.write(secret.value)
+        f.write(key)
+    os.chmod("/etc/nginx/ssl/server.crt", 0o644)
+    os.chmod("/etc/nginx/ssl/server.key", 0o600)
