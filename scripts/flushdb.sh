60a8a6bbfaf56ae9aba85308439852cba014ab8c
Andrii Shukalo <shukaload@gmail.com>
2026-09-02 23:09:51 +0300

    fix flushdb.sh script

--- scripts/flushdb.sh
+++ scripts/flushdb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cd /home/developer/Infra4BeautyAI/backend_service
+cd /home/developer/Infra4BeautyAI/services/backend_service
 docker compose down
 rm -rf /beautyDB/*
-cd /home/developer/Infra4BeautyAI/backend_service
+cd /home/developer/Infra4BeautyAI/services/backend_service
 docker compose up -d
